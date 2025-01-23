2ad24e9fc099e4f2494457640eac3ecd6ae76748
ams557 <ams557-macos@As-MacBook-Pro.local>
2025-01-22 21:54:03 -0500

    Put everything from nb to main

--- SRC/main.py
+++ SRC/main.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     criterion = torch.nn.CrossEntropyLoss()
     initial_loss = criterion(outputs.logits, labels)
     print("Initial loss:", initial_loss)
-    wandb_logger = WandbLogger(name='luke-first-run-12000-articles-bis', project='LUKE')
+    wandb_logger = WandbLogger(name='LUKE-N2C2-RE', project='LUKE')
     # for early stopping, see https://pytorch-lightning.readthedocs.io/en/1.0.0/early_stopping.html?highlight=early%20stopping
     early_stop_callback = EarlyStopping(
         monitor='val_loss',
@@ -49,3 +49,44 @@ if __name__ == "__main__":
     )
     trainer = Trainer(logger=wandb_logger, callbacks=[EarlyStopping(monitor='validation_loss')])
     trainer.fit(model)
+    trainer.test()
+    loaded_model.model.eval()
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    loaded_model.to(device)
+
+    predictions_total = []
+    labels_total = []
+    for batch in tqdm(test_dataloader):
+        # get the inputs;
+        labels = batch["label"]
+        del batch["label"]
+
+        # move everything to the GPU
+        for k,v in batch.items():
+            batch[k] = batch[k].to(device)
+
+        # forward pass
+        outputs = loaded_model.model(**batch)
+        logits = outputs.logits
+        predictions = logits.argmax(-1)
+        predictions_total.extend(predictions.tolist())
+        labels_total.extend(labels.tolist())
+    print("Accuracy on test set:", accuracy_score(labels_total, predictions_total))
+    loaded_model = LUKE.load_from_checkpoint(checkpoint_path="LUKE/checkpoints/epoch=3-step=7699.ckpt")
+    print(test_df.iloc[0].sentence)
+    idx = 2
+    text = test_df.iloc[idx].sentence
+    entity_spans = test_df.iloc[idx].entity_spans  # character-based entity spans
+    entity_spans = [tuple(x) for x in entity_spans]
+
+    inputs = tokenizer(text, entity_spans=entity_spans, return_tensors="pt")
+
+    outputs = loaded_model.model(**inputs)
+    logits = outputs.logits
+    predicted_class_idx = logits.argmax(-1).item()
+    print("Sentence:", text)
+    print("Ground truth label:", test_df.iloc[idx].string_id)
+    print("Predicted class idx:", id2label[predicted_class_idx])
+    print("Confidence:", F.softmax(logits, -1).max().item())
+    
